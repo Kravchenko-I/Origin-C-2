134f283805435798a55c707d8c2a793b9fb967e0
Igor <kravchenkoigor010687@gmail.com>
2023-02-04 11:57:31 +0100

    Create 3rd folder

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,3 +4,14 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if ((num % 7 == 0) && (num % 23 == 0)) 
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Нет!");
+}
