9d375adb835f7e5595d676f796636862f09bca0a
Igor <kravchenkoigor010687@gmail.com>
2023-02-04 12:07:56 +0100

    Create 4rd folder

--- task4/Program.cs
+++ task4/Program.cs
@@ -3,3 +3,16 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if ((num1 * num1 == num2) || (num2 * num2 == num1)) 
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Нет!");
+}
